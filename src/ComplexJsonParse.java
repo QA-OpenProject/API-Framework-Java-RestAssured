5d811ea17913ebd18c7edc210606d8b256fa81bd
walter <walter.zhangfang@gmail.com>
2024-04-07 17:25:22 +1200

    Add the staticJson file

--- src/ComplexJsonParse.java
+++ src/ComplexJsonParse.java
@@ -9,23 +9,22 @@ public class ComplexJsonParse {
 
 		// Print No of courses returned by API
 		int count = js.getInt("courses.size()");
-		System.out.println(count);
+		System.out.println("count of courses is: " + count);
 
 		// Print Purchase Amount
 		int totalAmount = js.getInt("dashboard.purchaseAmount");
-		System.out.println(totalAmount);
+		System.out.println("Total Aoumt is: " + totalAmount);
 
 		// Print Title of the first course
-		String titleFirstCourse = js.get("courses[2].title");
-		System.out.println(titleFirstCourse);
+		String titleFirstCourse = js.get("courses[0].title");
+		System.out.println("The first cousrse is: " + titleFirstCourse);
 
 		// Print All course titles and their respective Prices
 		for (int i = 0; i < count; i++) {
 			String courseTitles = js.get("courses[" + i + "].title");
+			
 			System.out.println(js.get("courses[" + i + "].price").toString());
-
 			System.out.println(courseTitles);
-
 		}
 
 		// Print no of copies sold by RPA Course
